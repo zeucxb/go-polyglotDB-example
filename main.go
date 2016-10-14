8ebdefcee9a8cf2d392fc9f8547b5d67e4d60279
Zeu Cxb <zeucxb@MacBook-Pro-de-Zeu.local>
2016-10-14 16:57:42 -0300

    Initial

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmcvetta/neoism"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func main() {
+	db := connect()
+	cq := neoism.CypherQuery{
+		Statement: `
+			MATCH (u:User)
+			WHERE u.ID = {id}
+			MATCH (u)-[f:Follow]->(following)
+			RETURN following.ID
+			`,
+		Parameters: neoism.Props{"id": "57eac14aee610931a7732d60"},
+		Result:     &result{},
+	}
+	db.Cypher(&cq)
+
+	r := cq.Result.(*result)
+
+	mongo := connectMongo("users")
+
+	var user mongoUser
+
+	for _, v := range *r {
+		mongo.FindId(v.ID).One(&user)
+
+		fmt.Println(user)
+	}
+}
+
+type result []struct {
+	ID bson.ObjectId `json:"following.ID" bson:"_id"`
+}
+
+type mongoUser struct {
+	Username string `bson:"username"`
+	Email    string `bson:"email"`
+}
+
+func connect() *neoism.Database {
+	db, err := neoism.Connect("http://neo4j:Zeu16051997@localhost:7474/db/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+// connectMongo return a mongo session
+func connectMongo(collectionName string) *mgo.Collection {
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://localhost")
+
+	// Check if connection error, is mongo runnig?
+	if err != nil {
+		panic(err)
+	}
+
+	return s.DB("onefits").C(collectionName)
+}
